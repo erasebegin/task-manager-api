0bbaf87449719821ef92b41f73eabf17a5a767a2
erasebegin <chris.james.haupt@gmail.com>
2020-10-22 20:02:08 +0100

    4th attempt

--- src/index.js
+++ src/index.js
@@ -20,16 +20,16 @@ app.use((req, res, next) => {
   }
 });
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin: http://localhost:3001");
-//   res.header(
-//     "Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin: *");
+  res.header(
+    "Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token"
+  );
+  next();
+});
 
 app.use(cors());
 
